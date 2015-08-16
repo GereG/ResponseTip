bdd3bee009442eb03cd6661fbaaf43d4a1e6c5c2
GereG <tomgardelka@gmail.com>
2015-08-16 17:33:43 +0200

    initial commit

---
+++ responseTip/responseTip/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="responseTip.MvcApplication" Language="C#" %>
---
+++ responseTip/responseTip/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(responseTip.Startup))]
+namespace responseTip
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
