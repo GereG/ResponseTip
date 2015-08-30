eec5408a79711749eaf109a6d02fb6836ef05230
GereG <tomgardelka@gmail.com>
2015-08-30 11:35:54 +0200

    task create default route

--- responseTip/responseTip/App_Start/RouteConfig.cs
+++ responseTip/responseTip/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace responseTip
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "ResponseTipTasks", action = "Create", id = UrlParameter.Optional }
             );
         }
     }
--- responseTip/responseTip/Models/SocialSiteUsers.cs
+++ responseTip/responseTip/Models/SocialSiteUsers.cs
@@ -4,5 +4,6 @@
     {
         public int SocialSiteUsersID { get; set; }
         public string UserName { get; set; }
+        public SupportedSocialSites socialSite { get; set; }
     }
 }
