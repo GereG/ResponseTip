c842b69e16d94f311b61f2873e468867ddcc9ff0
GereG <tomgardelka@gmail.com>
2015-11-03 22:59:09 +0100

    arbitrtown_project

---
+++ responseTip/ArbiterTown/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ArbiterTown.MvcApplication" Language="C#" %>
---
+++ responseTip/ArbiterTown/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ArbiterTown.Startup))]
+namespace ArbiterTown
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
